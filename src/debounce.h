f44c07a356f65f5c7ecb24eba432ac7165e73a29
jbellue <julien@bellue.fr>
2021-09-19 19:05:03 +0200

    Add credit to debounce.h

--- src/debounce.h
+++ src/debounce.h
@@ -1,6 +1,7 @@
 #ifndef DEBOUNCE_H
 #define DEBOUNCE_H
 
+// heavily inspired from https://www.avrfreaks.net/sites/default/files/forum_attachments/debounce.pdf
 #include <stdint.h>
 
 static inline uint8_t debounce(const uint8_t pin) {
@@ -9,10 +10,8 @@ static inline uint8_t debounce(const uint8_t pin) {
 
     const uint8_t current_state = (~PINB & (1 << pin)) != 0;
     if (current_state != previous_state) {
-        if (++count >= 3) {
-            // The button have not bounced for three checks, update state
+        if (++count >= 3) { // No bounce in three checks, update state
             previous_state = current_state;
-            // If the button was pressed (not released), tell main so
             count = 0;
             if (current_state != 0) {
                 return 1;
